e76708113e8336052615d49b9329e1d272d67182
EC2 Default User <ec2-user@ip-172-31-23-73.ec2.internal>
2022-04-11 16:52:39 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+        log_in @user
         flash[:success] = "Welcome to the Sample App!"
         redirect_to @user
     else
+      
       render 'new'
     end
   end
@@ -17,6 +18,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+
 
       private
 
--- test/helpers/users_helper.rb
+++ test/helpers/users_helper.rb
@@ -1,10 +1,8 @@
 module UsersHelper
+  fixtures :all
 
-  # 引数で与えられたユーザーのGravatar画像を返す
-  # def gravatar_for(user, size: 80)
-  #   gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-  #   size = options[:size]
-  #   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-  #   image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  # end
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
